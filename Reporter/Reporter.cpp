f1489c8fbb4c3e91c98c71f82abaf572ac75af76
vr009 <slavarianov@yandex.ru>
2021-04-14 01:36:08 +0300

    Reporter frame

---
+++ Reporter/Reporter.cpp
@@ -0,0 +1,12 @@
+#include "Reporter.h"
+
+Reporter::Reporter() {}
+
+Reporter::~Reporter() {}
+
+void Reporter::WorkCycle() {}
+
+Solution Reporter::GetSolution(int) {}
+
+void Reporter::SetUp(std::shared_ptr<Queue<Result*>> &results, std::unique_ptr<Database> db, std::unique_ptr<ParserGAtoHuman> parser) {}
+
---
+++ Reporter/Reporter.h
@@ -0,0 +1,32 @@
+#ifndef TABLEOFLIFE_REPORTER_H
+#define TABLEOFLIFE_REPORTER_H
+
+#include <memory>
+
+class GABuilder;
+class Database;
+class ParserGAtoHuman;
+class Queue;
+struct Solution;
+
+class Reporter {
+public:
+
+    Reporter();
+    ~Reporter();
+
+    void SetUp(std::shared_ptr<Queue<Result*>> &results, std::unique_ptr<Database> db, std::unique_ptr<ParserGAtoHuman> parser);
+    void WorkCycle();
+    Solution GetSolution(int);
+
+private:
+    Reporter(const Reporter & a) = delete;
+    Reporter& operator=(const Reporter & a) = delete;
+
+    std::shared_ptr<Queue<Result*>> rque;
+    std::unique_ptr<Database> db;
+    std::unique_ptr<ParserGAtoHuman> parser;
+};
+
+
+#endif //TABLEOFLIFE_REPORTER_H
