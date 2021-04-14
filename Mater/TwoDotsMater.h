bb4d90fac24fbb6c904f5e6dfd96af3aba0bf099
k-t-l-h <laciedreamer@gmail.com>
2021-04-15 02:50:11 +0300

    choose

---
+++ Mater/TwoDotsMater.h
@@ -0,0 +1,19 @@
+#ifndef TABLEOFLIFE_TWODOTSMATER_H
+#define TABLEOFLIFE_TWODOTSMATER_H
+
+template< std::size_t N >
+class TwoPointsMater: public IMater<N> {
+public:
+  explicit TwoPointsMater(float pr): probability(pr){};
+  ~TwoPointsMater() =default;
+  std::vector<Genome<N>*>  Mate(Genome<N>*, Genome<N>*) override;
+private:
+  float probability;
+};
+
+template <std::size_t N>
+std::vector<Genome<N>*> TwoPointsMater<N>::Mate(Genome<N> *, Genome<N> *) {
+return std::vector<Genome<N>*>(2);
+}
+
+#endif // TABLEOFLIFE_TWODOTSMATER_H
