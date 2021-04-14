0a0c7cc58555e42fb68273503e425ab29a6d660f
k-t-l-h <laciedreamer@gmail.com>
2021-04-15 02:56:01 +0300

    mooore

--- Mutator/Mutator_test.cpp
+++ Mutator/Mutator_test.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "../Mutator/SimpleMutator.h"
+#include "../Mutator/ReverseMutator.h"
 
 //что будет если передать null
 TEST(TEST_MUTATOR, zero) {
@@ -11,6 +12,16 @@ TEST(TEST_MUTATOR, zero) {
   ASSERT_EQ(answer, nullptr);
 
 }
+
+TEST(TEST_MUTATOR, zero_random) {
+  const std::size_t N = sizeof(int);
+  ReverseMutator<N> m(0);
+  //нет так нет
+  ASSERT_NO_THROW(m.Mutate(nullptr));
+  auto answer = m.Mutate(nullptr);
+  ASSERT_EQ(answer, nullptr);
+}
+
 
 TEST(TEST_MUTATOR, zero2) {
   const std::size_t N = sizeof(int);
@@ -24,6 +35,19 @@ TEST(TEST_MUTATOR, zero2) {
   ASSERT_EQ(answer.GetGene(0), g.GetGene(0));
 }
 
+TEST(TEST_MUTATOR, zero2_random) {
+  const std::size_t N = sizeof(int);
+  float probability = 0;
+  //при вероятности мутации в 0 мутации не должно быть
+  ReverseMutator<N> rm(probability);
+
+  Genome<N> g(1);
+  auto answer = rm.Mutate(&g);
+  ASSERT_NE(answer, nullptr);
+  ASSERT_EQ(answer.GetGene(0), g.GetGene(0));
+}
+
+
 TEST(TEST_MUTATOR, for_sure) {
   const std::size_t N = sizeof(int);
   float probability = 1;
@@ -33,6 +57,19 @@ TEST(TEST_MUTATOR, for_sure) {
   //мутация возвращает копию или новый объект
   auto answer = sm.Mutate(&g);
   ASSERT_NE(answer->GetGene(0), g.GetGene(0));
+}
+
+TEST(TEST_MUTATOR, for_sure_random) {
+  const std::size_t N = sizeof(int);
+  float probability = 1;
+  int value = 5;
+  //при вероятности мутации в 1 мутация обязана произойти
+  ReverseMutator<N> rm(probability);
+  Genome<N> g(1);
+  g.SetGene(0, value);
+  //мутация возвращает копию или новый объект
+  auto answer = rm.Mutate(&g);
+  ASSERT_NE(answer->GetGene(0), value);
 }
 
 int main(int argc, char** argv) {
---
+++ Mutator/ReverseMutator.h
@@ -0,0 +1,22 @@
+#ifndef TABLEOFLIFE_REVERSEMATER_H
+#define TABLEOFLIFE_REVERSEMATER_H
+
+#include "IMutator.h"
+
+//реверсит все биты
+template <std::size_t N>
+class ReverseMutator : public IMutator<N> {
+public:
+  explicit ReverseMutator(float probability) : probability(probability){};
+  ~ReverseMutator() = default;
+  Genome<N>* ReverseMutator(Genome<N>*) override;
+
+private:
+  float probability;
+};
+
+template <std::size_t N>
+Genome<N>* ReverseMutator<N>::Mutate(Genome<N>* genome) {
+  return genome;
+}
+#endif // TABLEOFLIFE_REVERSEMATER_H
