e5e24f1c11362aa66ddc116249acc62736468eda
vr009 <slavarianov@yandex.ru>
2021-04-14 22:44:29 +0300

    DB class wrapper added

---
+++ Database/Database.cpp
@@ -0,0 +1,10 @@
+//
+// Created by slava on 14.04.2021.
+//
+
+#include "Database.h"
+
+Database::Database() {}
+Database::~Database() {}
+void Database::Insert() {}
+bool Database::Select(const char *options) {}
---
+++ Database/Database.h
@@ -0,0 +1,19 @@
+#ifndef TABLEOFLIFE_DATABASE_H
+#define TABLEOFLIFE_DATABASE_H
+
+class sql;
+
+class Database {
+public:
+    Database();
+    ~Database();
+
+    bool Select(const char * options);
+    void Insert();
+
+private:
+    sql *connection;
+};
+
+
+#endif //TABLEOFLIFE_DATABASE_H
