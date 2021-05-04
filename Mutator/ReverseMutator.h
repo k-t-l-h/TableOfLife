99cfcb65d4ae613568de5763e89f9eafa6b54b39
k-t-l-h <laciedreamer@gmail.com>
2021-05-04 19:32:47 +0300

    Mutationssss

--- Mutator/ReverseMutator.h
+++ Mutator/ReverseMutator.h
@@ -7,16 +7,39 @@
 template <std::size_t N>
 class ReverseMutator : public IMutator<N> {
 public:
-  explicit ReverseMutator(float probability) : probability(probability){};
-  ~ReverseMutator() = default;
-  Genome<N>* Mutate(Genome<N>*) override;
+    explicit ReverseMutator(float probability, std::size_t variants)
+            : probability(probability), variants(variants){};
+    ~ReverseMutator() = default;
+    Genome<N>* Mutate(Genome<N>*) override;
 
 private:
-  float probability;
+    float probability;
+    std::size_t variants;
 };
 
 template <std::size_t N>
 Genome<N>* ReverseMutator<N>::Mutate(Genome<N>* genome) {
-  return genome;
+    if (genome == nullptr) {
+        return nullptr;
+    }
+
+    std::size_t value = genome->GetGenes().size();
+    Genome<N>* mutant = new Genome<N>(value);
+
+    // TODO: вынести в оператор копирования
+    for (int i = 0; i < value; ++i) {
+        mutant->SetGenes(genome->GetGenes());
+    }
+
+    auto check = rand();
+    float rands = float(check) / (float)RAND_MAX;
+    if (rands < probability) {
+        // TODO: тут возможно проще сразу все гены изменить
+        for (int i = 0; i < value; ++i) {
+            mutant->SetGene(i, (mutant->GetGene(i) xor rand()) % variants);
+        }
+    }
+
+    return mutant;
 }
-#endif // TABLEOFLIFE_REVERSEMATER_H
+#endif  // TABLEOFLIFE_REVERSEMATER_H
--- Mutator/SimpleMutator.h
+++ Mutator/SimpleMutator.h
@@ -2,20 +2,42 @@
 #define TABLEOFLIFE_SIMPLEMUTATOR_H
 #include "IMutator.h"
 
-
 template <std::size_t N>
 class SimpleMutator : public IMutator<N> {
-public:
-  explicit SimpleMutator(float probability) : probability(probability){};
+ public:
+  explicit SimpleMutator(float probability, std::size_t variants)
+      : probability(probability), variants(variants){};
   ~SimpleMutator() = default;
   Genome<N>* Mutate(Genome<N>*) override;
 
-private:
+ private:
   float probability;
+  std::size_t variants;
 };
 
 template <std::size_t N>
 Genome<N>* SimpleMutator<N>::Mutate(Genome<N>* genome) {
-  return genome;
+  if (genome == nullptr) {
+    return nullptr;
+  }
+
+  std::size_t value = genome->GetGenes().size();
+  Genome<N>* mutant = new Genome<N>(value);
+
+  // TODO: вынести в оператор копирования
+  for (int i = 0; i < value; ++i) {
+    mutant->SetGenes(genome->GetGenes());
+  }
+
+  auto check = rand();
+  float rands = float(check) / (float)RAND_MAX;
+  if (rands < probability) {
+    size_t position = rand() % value;
+    auto v = mutant->GetGene(position);
+    v = v xor check;
+    mutant->SetGene(position, v % variants);
+  }
+
+  return mutant;
 }
 #endif  // TABLEOFLIFE_SIMPLEMUTATOR_H
