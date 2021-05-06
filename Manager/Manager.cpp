63dcf94e6575357a9191b2322a5330fa0f0915df
vr009 <slavarianov@yandex.ru>
2021-05-06 17:49:57 +0300

    Manager + reporter sketch

--- Manager/Manager.cpp
+++ Manager/Manager.cpp
@@ -8,15 +8,17 @@ class GABuilder{
 public:
     GABuilder(){}
 
-private:
-    int a;
 };
 
 struct Request{
     int id;
     int type;
     Request(const Request &r){}
+};
 
+
+struct Result{
+    int id;
 };
 
 Manager::Manager(std::shared_ptr<std::queue<Request*>> &tasq, std::shared_ptr<std::queue<Result*>> &resq ) : tque(tasq), rque(resq) {}
@@ -27,7 +29,7 @@ void Manager::WorkCycle() {
     while(true){
         auto current_task = std::make_unique<Request>( *tque->front() );
         tque->pop();
-        std::thread t(work,std::move(current_task));
+        std::thread t(&Manager::work,std::move(current_task));
         t.detach();
     }
 }
@@ -39,7 +41,7 @@ void Manager::SetBuilder() {
 
 //use mutex or cond var
 void Manager::work( std::unique_ptr<Request> task ) {
-    std::unique_ptr<Result*> result;
+    std::unique_ptr<Result> result;
     /*TODO switch case*/
     if(task->type == 1){
         //......
