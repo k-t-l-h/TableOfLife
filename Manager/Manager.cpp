3e3c3af0908cf5dedf447760d88936dc374ec9cd
vr009 <slavarianov@yandex.ru>
2021-05-06 15:06:33 +0300

    Manager class sketch

--- Manager/Manager.cpp
+++ Manager/Manager.cpp
@@ -1,10 +1,41 @@
 #include "Manager.h"
+#include <thread>
+#include <memory>
+#include <atomic>      // condition variables
+#include <mutex>
+
+Manager(std::shared_ptr<std::queue<Request*>> &tasq, std::shared_ptr<std::queue<Result*>> &resq):tque(tasq), rque(resq) {}
+
+std::mutex rque_mutex;
+
+void Manager::WorkCycle() {
+    while(true){
+        auto current_task = std::make_unique( tque->top() );
+        std::thread t(work,std::move(current_task));
+        t.detach();
+    }
+}
+
+void Manager::SetBuilder(std::unique_ptr<GABuilder> builder) {
+    builder = std::make_unique(new GABuilder());
+}
 
 
-void Manager::Setup(std::shared_ptr <Queue> &tasq, std::shared_ptr <Queue> &resq) {}
+//use mutex or cond var
+void Manager::work( std::unique_ptr<Request*> task ) {
+    std::unique_ptr<Result*> result;
+    /*TODO switch case*/
+    if(task->type == 1){
+        //......
+    } else{
+        //......
+    }
 
-void Manager::WorkCycle() {}
-
-void Manager::SetBuilder(std::unique_ptr<GABuilder> &builder) {}
-
-const Result * Manager::work(std::string, std::string, std::string, std::string) {}
+    if( result.get() != nullptr){
+        //TODO atomic to use or std::guard_lock
+        const std::lock_guard<std::mutex> lock(rque_mutex);
+        rque->push(std::move(result));
+    } else {
+        //throw smth;
+    }
+}
--- Manager/Manager.h
+++ Manager/Manager.h
@@ -2,33 +2,35 @@
 #define TABLEOFLIFE_MANAGER_H
 
 #include <memory>
+#include <vector>
+#include <queue>
 
 
 class GABuilder;
-class Result;
-class Request;
+struct Result;
+struct Request;
 
 template<class T> class Queue;
 
 class Manager {
 public:
-    Manager() = default;
+
+    Manager(std::shared_ptr<std::queue<Request*>> &tasq, std::shared_ptr<std::queue<Result*>> &resq);
     ~Manager() = default;
 
-    void Setup(std::shared_ptr<Queue<Request*>> &tasq, std::shared_ptr<Queue<Result*>> &resq);
     void SetBuilder(std::unique_ptr<GABuilder> &builder);
     void WorkCycle();
 
 private:
-
-    Manager& operator=() = delete;
+    Manager& operator=(Manager &a) = delete;
+    Manager() = delete;
     Manager(Manager &a) = delete;
 
     const Result* work(std::string, std::string, std::string, std::string);
 
     std::unique_ptr<GABuilder> builder;
-    std::shared_ptr<Queue<Request*>> tque;                            //t - task
-    std::shared_ptr<Queue<Result*>> rque;                            //r - result
+    std::shared_ptr<std::queue<Request*>> &tque;                            //t - task
+    std::shared_ptr<std::queue<Result*>> &rque;                            //r - result
 };
 
 
