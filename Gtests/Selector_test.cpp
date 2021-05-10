16f02eb60b87bc0db1efcbdf7867b356b46e1d29
k-t-l-h <laciedreamer@gmail.com>
2021-05-10 19:23:25 +0300

    test

--- Gtests/Selector_test.cpp
+++ Gtests/Selector_test.cpp
@@ -10,7 +10,7 @@ TEST(TEST_SELECTION, nullptr_test){
   const std::size_t max = 2;
 
   Selector<N> s;
-  ASSERT_NO_THROW(s.Select(nullptr));
+  ASSERT_NO_THROW(s.Select(std::vector<Genome<N>*>()));
 }
 
 //при передаче nullptr не должно быть ошибок
@@ -19,7 +19,7 @@ TEST(TEST_SELECTION, nullptr_test){
   const std::size_t max = 2;
 
   TopSelector<N> s;
-  ASSERT_NO_THROW(s.Select(nullptr));
+  ASSERT_NO_THROW(s.Select(std::vector<Genome<N>*>()));
 }
 
 //при передаче пустого вектора не должно быть ошибок
@@ -30,7 +30,7 @@ TEST(TEST_SELECTION, empty_test){
   Selector<N> s;
   SimpleCreator<N> sc;
   auto v = sc.Create(max, max, max);
-  ASSERT_NO_THROW(s.Select(&v));
+  ASSERT_NO_THROW(s.Select(v));
 }
 
 //при передаче пустого вектора не должно быть ошибок
@@ -41,7 +41,7 @@ TEST(TEST_SELECTION, empty_test_top){
   TopSelector<N> ts;
   SimpleCreator<N> sc;
   auto v = sc.Create(max, max, max);
-  ASSERT_NO_THROW(ts.Select(&v));
+  ASSERT_NO_THROW(ts.Select(v));
 }
 
 //при передаче пустого вектора не должно быть ошибок
@@ -52,7 +52,7 @@ TEST(TEST_SELECTION, empty_test_2){
   Selector<N> s;
   SimpleCreator<N> sc;
   auto v = sc.Create(max, max, max);
-  auto g = s.Select(&v);
+  auto g = s.Select(v);
   ASSERT_EQ(g, nullptr);
 
 }
@@ -65,7 +65,7 @@ TEST(TEST_SELECTION, empty_test_2_top){
   TopSelector<N> s;
   SimpleCreator<N> sc;
   auto v = sc.Create(max, max, max);
-  auto g = s.Select(&v);
+  auto g = s.Select(v);
   ASSERT_EQ(g, nullptr);
 }
 
@@ -79,7 +79,7 @@ TEST(TEST_SELECTION, one_test){
   SimpleCreator<N> sc;
   auto v = sc.Create(max, max, max);
   auto eq = v[0];
-  auto g = s.Select(&v);
+  auto g = s.Select(v);
   ASSERT_EQ(g, eq);
   ASSERT_EQ(v.size(), 0);
 
@@ -95,7 +95,7 @@ TEST(TEST_SELECTION, one_test_top){
   SimpleCreator<N> sc;
   auto v = sc.Create(max, max, max);
   auto eq = v[0];
-  auto g = s.Select(&v);
+  auto g = s.Select(v);
   ASSERT_EQ(g, eq);
   ASSERT_EQ(v.size(), 0);
 
@@ -112,7 +112,7 @@ TEST(TEST_SELECTION, all_test){
   auto v = sc.Create(max, max, max);
   auto eq = v[1];
   v[1]->SetFitness(1);
-  auto g = s.Select(&v);
+  auto g = s.Select(v);
   ASSERT_EQ(g, eq);
   ASSERT_EQ(v.size(), 1);
 }
@@ -126,7 +126,7 @@ TEST(TEST_SELECTION, all_test_top){
   auto v = sc.Create(max, max, max);
   auto eq = v[1];
   v[1]->SetFitness(1);
-  auto g = s.Select(&v);
+  auto g = s.Select(v);
   ASSERT_EQ(g, eq);
   ASSERT_EQ(v.size(), 1);
 }
