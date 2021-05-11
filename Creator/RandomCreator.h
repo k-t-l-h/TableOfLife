97f591ea27d50b8e4e95f56cc42f3da3aa68c758
k-t-l-h <laciedreamer@gmail.com>
2021-05-11 16:04:53 +0300

    multi thread work

--- Creator/RandomCreator.h
+++ Creator/RandomCreator.h
@@ -1,8 +1,9 @@
 #ifndef TABLEOFLIFE_RANDOMCREATOR_H
 #define TABLEOFLIFE_RANDOMCREATOR_H
 
-#include "ICreator.h"
 #include "../Genome/Genome.h"
+#include "ICreator.h"
+#include <thread>
 
 //Рандомно создает гены
 template <std::size_t N>
@@ -12,22 +13,62 @@ public:
     ~RandomCreator() = default;
     std::vector<Genome<N> *> Create(std::size_t all, std::size_t length,
                                     std::size_t vars) override;
+
+private:
+    static void GenerateInThread(std::vector<Genome<N>*> *generation,
+                          std::size_t num, std::size_t vars, std::size_t length);
 };
 
-template <std::size_t N>
-std::vector<Genome<N> *> RandomCreator<N>::Create(std::size_t all,
-                                                  std::size_t length,
-                                                  std::size_t vars) {
-    auto *generation = new std::vector<Genome<N> *>();
 
-    for (std::size_t i = 0; i < all; ++i) {
+template <std::size_t N>
+void RandomCreator<N>::GenerateInThread(std::vector<Genome<N>*> *generation,
+                                        std::size_t num, std::size_t vars, std::size_t length){
+
+    for (size_t i = 0; i < num; ++i) {
         auto genome = new Genome<N>(length);
+
         for (std::size_t j = 0; j < length; ++j) {
             auto value = static_cast<std::size_t>(rand() % vars);
             genome->SetGene(j, value);
         }
         generation->push_back(genome);
     }
+};
+
+template <std::size_t N>
+std::vector<Genome<N> *> RandomCreator<N>::Create(std::size_t all,
+                                                  std::size_t length,
+                                                  std::size_t vars) {
+
+    //создаем вектор геномов
+    auto *generation = new std::vector<Genome<N> *>();
+    //выделяем ему фиксированное количество элементов
+    generation->reserve(all);
+
+    //смотрим сколько можем создать тредов
+    auto nthreads = std::thread::hardware_concurrency();
+
+    //TODO: здесь обнаружилась ошибка с числом тредов
+    //TODO: нужно её использовать
+    std::size_t  parts = all / nthreads;
+    nthreads = parts == 0 ? all : nthreads;
+    parts = parts > 0 ? parts: 1;
+    std::vector<std::thread> threads;
+
+    for (int i = 0; i < nthreads; ++i) {
+
+        std::thread t(GenerateInThread, std::ref(generation),
+                      parts,vars, length);
+        threads.push_back(std::move(t));
+    }
+
+
+    for (std::thread & th : threads)
+    {
+        if (th.joinable())
+            th.join();
+    }
+
     return *generation;
 }
 
--- Creator/SimpleCreator.h
+++ Creator/SimpleCreator.h
@@ -12,23 +12,61 @@ public:
     //первый параметр - сколько создавать, второй - сколько вариантов одного гена
     std::vector<Genome<N> *> Create(std::size_t all, std::size_t length,
                                     std::size_t vars) override;
+
+private:
+    static void GenerateInThread(std::vector<Genome<N>*> *generation,
+                                 std::size_t num, std::size_t vars, std::size_t length,
+                                 std::size_t offset);
 };
 
 template <std::size_t N>
 std::vector<Genome<N> *> SimpleCreator<N>::Create(std::size_t all,
                                                   std::size_t length,
                                                   std::size_t vars) {
-    auto *generation = new std::vector<Genome<N> *>();
 
-    for (std::size_t i = 0; i < all; ++i) {
+    //создаем вектор геномов
+    auto *generation = new std::vector<Genome<N> *>();
+    //выделяем ему фиксированное количество элементов
+    generation->reserve(all);
+
+    //смотрим сколько можем создать тредов
+    auto nthreads = std::thread::hardware_concurrency();
+
+    std::size_t  parts = all / nthreads;
+    nthreads = parts == 0 ? all : nthreads;
+    parts = parts > 0 ? parts: 1;
+    std::vector<std::thread> threads;
+
+    for (int i = 0; i < nthreads; ++i) {
+        std::thread t(GenerateInThread, std::ref(generation),
+                      parts,vars, length, parts*i);
+        threads.push_back(std::move(t));
+    }
+
+
+    for (std::thread & th : threads)
+    {
+        if (th.joinable())
+            th.join();
+    }
+
+    return *generation;
+}
+template<std::size_t N>
+void SimpleCreator<N>::GenerateInThread(std::vector<Genome<N> *> *generation,
+                                        std::size_t num,
+                                        std::size_t vars,
+                                        std::size_t length, std::size_t offset) {
+
+    for (size_t i = 0; i < num; ++i) {
         auto genome = new Genome<N>(length);
+
         for (std::size_t j = 0; j < length; ++j) {
-            auto value = static_cast<std::size_t>((i + j) % vars);
+            auto value = static_cast<std::size_t>((i + offset + j) % vars);
             genome->SetGene(j, value);
         }
         generation->push_back(genome);
     }
-    return *generation;
 }
 
 #endif  // TABLEOFLIFE_SIMPLECREATOR_H
