3ba2b7ac7ed1c05fe88b24d70f17dcffc4921160
vr009 <slavarianov@yandex.ru>
2021-05-13 01:08:13 +0300

    working adapter

--- Adapter/Adapter.cpp
+++ Adapter/Adapter.cpp
@@ -1,10 +1,12 @@
 #include "Adapter.h"
+#include "IDatabase.h"
 
-const std::vector<int>& Adapter::GetResult(int) const {}
+const std::vector<int> Adapter::GetResult(int id) const {
+    return db->Select(id);
+}
 
 Adapter::Adapter() {}
 
-Adapter::Adapter(std::shared_ptr <IDatabase> &t_db) {}
+Adapter::Adapter(std::shared_ptr <IDatabase> t_db): db(t_db) {}
 
 Adapter::~Adapter() {}
-
