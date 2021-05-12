6463fead6dd5dcf84bc9741a8cef94e243d5527a
vr009 <slavarianov@yandex.ru>
2021-05-12 20:03:29 +0300

    build passing

--- Adapter/Adapter.cpp
+++ Adapter/Adapter.cpp
@@ -4,7 +4,7 @@ const std::vector<int>& Adapter::GetResult(int) const {}
 
 Adapter::Adapter() {}
 
-Adapter::Adapter(std::shared_ptr <Database> &t_db) {}
+Adapter::Adapter(std::shared_ptr <IDatabase> &t_db) {}
 
 Adapter::~Adapter() {}
 
