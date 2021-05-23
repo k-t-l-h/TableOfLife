7f1dc4abe8db25ebd0f33e3c523b3b79bb5c1e09
vr009 <slavarianov@yandex.ru>
2021-05-23 05:48:15 +0300

    fix of result

--- Result/Result.h
+++ Result/Result.h
@@ -4,11 +4,12 @@
 #include <vector>
 #include "../Request/Request.h"
 #include <boost/uuid/uuid.hpp>            // uuid class
+#include <boost/uuid/uuid_generators.hpp>
 
 namespace u = boost::uuids;
 
 struct Result {
-    u::uuid id;
+    u::uuid id = u::nil_uuid();
     std::vector<size_t> result;
     std::vector<Classes> classes; // все занятия, а вних id группы
     size_t ClassesNumber;
